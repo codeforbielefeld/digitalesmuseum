29241836f3c21e58b1945b8a58986e7417dc4e9d
lmicke <LukasMicke96@gmail.com>
2020-11-12 20:28:39 +0100

    Added untested google cloud function for processing an image

---
+++ example-function/main.py
@@ -0,0 +1,31 @@
+from flask import escape
+from flask import Flask, request, Response
+import jsonpickle
+import numpy as np
+import cv2
+
+def image_http(request):
+    """HTTP Cloud Function.
+    Args:
+        request (flask.Request): The request object.
+        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
+    Returns:
+        The response text, or any set of values that can be turned into a
+        Response object using `make_response`
+        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
+    """
+    r = request
+
+    nparr = np.fromstring(r.data, np.uint8)
+    # decode image
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+    # do some fancy processing here....
+
+    # build a response dict to send back to client
+    response = {'message': 'image received. size={}x{}'.format(img.shape[1], img.shape[0])
+                }
+    # encode response using jsonpickle
+    response_pickled = jsonpickle.encode(response)
+
+    return response_pickled
